25fe12d430a81de035b27a803a1b1929ab0d7ad5
umapoornima <root@ip-172-31-29-48.ec2.internal>
2022-10-11 10:19:22 +0000

    script to create 10 containers

---
+++ docker/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in {1..9}
+do
+	if [$( docker ps -a )];
+	then
+		echo "exists"
+	else
+	        docker container run -it --name container$i -p 500$i:80 nginx:latest bash
+	fi
+done
